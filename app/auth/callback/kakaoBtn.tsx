6979d556df3f17f5ecbbb434476cbb673c36c802
Jungwoo Hong <ghost1434@inu.ac.kr>
2025-01-07 23:18:17 +0900

    feat: oAuth를 통한 카카오 로그인 구현

---
+++ app/auth/callback/kakaoBtn.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+
+export default function KakaoButton() {
+  const supabase = createClient();
+
+  const signInWithKakao = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+      options: { redirectTo: "http://localhost:3000/auth/callback" },
+    });
+  };
+
+  return (
+    <button
+      onClick={signInWithKakao}
+      className="w-full rounded-xl bg-slate-100 px-4 py-2"
+    >
+      카카오 로그인
+    </button>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { cookies } from "next/headers";
+import { createClient } from "@supabase/supabase-js";
+import KakaoButton from "./auth/callback/kakaoBtn";
 
 export default async function Home() {
   return (
@@ -9,6 +12,7 @@ export default async function Home() {
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <KakaoButton />
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
       </main>
     </>
